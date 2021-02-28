f1581f1bbdd4e123a6738d2361a9052d3ec9ec0c
huosk <751933592@qq.com>
2021-02-28 13:11:11 +0800

    add query verb

--- TileDownloader/Program.cs
+++ TileDownloader/Program.cs
@@ -12,7 +12,8 @@ using CommandLine;
 
 namespace TileDownloader
 {
-    class Options
+    [Verb("load", isDefault: true)]
+    class LoadOptions
     {
         [Option("bound", Min = 4, HelpText = "download tiles bound,defined as a set of wgs84 longitudes and latitudes,order:south latitude,west longitude,north latitude,east longitude")]
         public IEnumerable<double> bound { get; set; }
@@ -40,6 +41,16 @@ namespace TileDownloader
 
         [Option("thread", HelpText = "worker thread count,default is 32", Default = 32)]
         public int thread { get; set; }
+    }
+
+    [Verb("query", HelpText = "query tile info")]
+    class QueryOption
+    {
+        [Option("positions", Min = 3, HelpText = "wgs84 latitudes and longitudes,format:: lon1,lat1,lod1,lon2,lat2,lod2,lon3,lat3,lod3")]
+        public IEnumerable<double> positions { get; set; }
+
+        [Option("tiles", Min = 3, HelpText = "tile index in web mercator projected map,format x1,y1,lod1,x2,y2,lod2,x3,y3,lod3")]
+        public IEnumerable<int> tileIds { get; set; }
     }
 
     class Program
@@ -77,8 +88,9 @@ namespace TileDownloader
 
         static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(BeginDownload)
+            Parser.Default.ParseArguments<LoadOptions, QueryOption>(args)
+                .WithParsed<LoadOptions>(HandleDownload)
+                .WithParsed<QueryOption>(HandleQuery)
                 .WithNotParsed(HandleParseError);
         }
 
@@ -90,7 +102,88 @@ namespace TileDownloader
             }
         }
 
-        private static void BeginDownload(Options options)
+        private static void HandleQuery(QueryOption options)
+        {
+            List<TileKey> tiles = new List<TileKey>();
+
+            double[] positions = options.positions.ToArray();
+            int[] tileIds = options.tileIds.ToArray();
+
+            for (int i = 0; i < positions.Length; i += 3)
+            {
+                if (i + 2 < positions.Length)
+                {
+                    double lon = positions[i];
+                    double lat = positions[i + 1];
+                    int lod = Convert.ToInt32(positions[i + 2]);
+
+                    var t = TileUtil.LatLongToTile(lat, lon, lod);
+                    tiles.Add(t);
+                }
+            }
+
+            for (int i = 0; i < tileIds.Length; i += 3)
+            {
+                if (i + 2 < tileIds.Length)
+                {
+                    int tileX = tileIds[i];
+                    int tileY = tileIds[i + 1];
+                    int tileLod = tileIds[i + 2];
+
+                    tiles.Add(new TileKey(tileX, tileY, tileLod));
+                }
+            }
+
+            StringBuilder outputBuilder = new StringBuilder();
+            string title_ = "Tile  Info";
+            int padLen = (int)((100 - title_.Length) >> 1);
+            outputBuilder.AppendLine($"{new string('-', padLen)}{title_}{new string('-', padLen)}");
+            outputBuilder.Append("Number".PadRight(10));
+            outputBuilder.Append("Lon".PadRight(12));
+            outputBuilder.Append("Lat".PadRight(12));
+            outputBuilder.Append("X".PadRight(10));
+            outputBuilder.Append("Y".PadRight(10));
+            outputBuilder.Append("Z".PadRight(6));
+            outputBuilder.Append("East".PadRight(10));
+            outputBuilder.Append("South".PadRight(10));
+            outputBuilder.Append("West".PadRight(10));
+            outputBuilder.Append("North".PadRight(10));
+            outputBuilder.AppendLine();
+
+            for (int i = 0; i < tiles.Count; i++)
+            {
+                double lon_ = 0.0;
+                double lat_ = 0.0;
+                double south_ = 0.0;
+                double west_ = 0.0;
+                double north_ = 0.0;
+                double east_ = 0.0;
+
+                TileUtil.TileToLatLon(tiles[i].X, tiles[i].Y, tiles[i].Lod, out lat_, out lon_);
+
+                TileUtil.TileToLatLon(tiles[i].WestBound, tiles[i].NorthBound, tiles[i].Lod, out north_, out east_);
+                TileUtil.TileToLatLon(tiles[i].EastBound, tiles[i].SouthBound, tiles[i].Lod, out south_, out west_);
+
+                outputBuilder.Append((i + 1).ToString().PadRight(10));
+                outputBuilder.Append(lon_.ToString("f5").PadRight(12));
+                outputBuilder.Append(lat_.ToString("f5").PadRight(12));
+                outputBuilder.Append(tiles[i].X.ToString().PadRight(10));
+                outputBuilder.Append(tiles[i].Y.ToString().PadRight(10));
+                outputBuilder.Append(tiles[i].Lod.ToString().PadRight(6));
+                outputBuilder.Append(east_.ToString("f5").PadRight(10));
+                outputBuilder.Append(south_.ToString("f5").PadRight(10));
+                outputBuilder.Append(west_.ToString("f5").PadRight(10));
+                outputBuilder.Append(north_.ToString("f5").PadRight(10));
+
+                outputBuilder.AppendLine();
+            }
+
+            outputBuilder.AppendLine(new string('-', 100));
+
+            Console.WriteLine(outputBuilder.ToString());
+        }
+
+        private static void HandleDownload(LoadOptions options)
         {
             if (options == null)
                 throw new ArgumentNullException("options");
@@ -240,7 +333,7 @@ namespace TileDownloader
 
                 await Task.Run(() => curl.WaitForExit());
 
-                if (curl.ExitCode != 0) 
+                if (curl.ExitCode != 0)
                 {
                     failed.Add(key);
                 }
